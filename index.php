4cd64dc28d1fbf572defeacdd51c107824f76e21
Adrián Rivera <aarivera7@utpl.edu.ec>
2022-11-08 12:23:51 -0500

    Se completo Adopciones y Veterinarios
    Se completo las secciones de Adopciones y Veterinarios, sin arreglar el
    color y los margenes de la foto.

--- index.php
+++ index.php
@@ -31,17 +31,48 @@
 		<h2>Adopción</h2>
 	</section>
 	<section class="adopcion1">
-		<img src="images/dog1.jpg">
+		<img src="images/dog1.jpg" alt="perro grande">
 		<section class="cardog">
 			<h3>Firulais</h3>
 			<p>Snares are silent killers laid by poachers across wild places to trap and kill animals for the illegal bushmeat tradeSnares are silent killers laid by poachers across wild places to trap and kill animals for the illegal bushmeat trade</p>
 			<button>Mas información</button>
 		</section>
 	</section>
-	<section class="adopcion2"><h3>Noticia 2</h3>
-		<a href="">enlace</a></section>
-	<section class="doctor">
-		<h3>Doctores</h3>
+	<section class="adopcion2">
+		<img src="images/dog2.jpg" alt="perro tierno">
+		<section class="cardog">
+			<h3>Noticia 2</h3>
+			<p>
+				Snares are silent killers laid by poachers across wild places to trap and kill animals for the illegal bushmeat tradeSnares are silent killers laid by poachers across wild places to trap and kill animals for the illegal bushmeat trade
+			</p>
+			<button>Mas información</button>
+		</section>
+	</section>
+	<section class="titulo1">
+		<h2>Doctores</h2>
+	</section>
+	<section class="doctores">
+		<section class="doctor doctor1">
+			<img src="images/johny.png" alt="doctor1">
+			<article class="descripcion">
+				<h3>John Harries</h3>
+				<p>Snares are silent killers laid by poachers across wild places to trap and kill animals for the illegal bushmeat tradeSnares are silent killers laid by poachers across wild places to trap and kill animals for the illegal bushmeat trade</p>
+			</article>
+		</section>
+		<section class="doctor doctor2">
+			<img src="images/doctora.png" alt="doctora1">
+			<article class="descripcion">
+				<h3>Sofia Simpson</h3>
+				<p>Snares are silent killers laid by poachers across wild places to trap and kill animals for the illegal bushmeat tradeSnares are silent killers laid by poachers across wild places to trap and kill animals for the illegal bushmeat trade</p>
+			</article>
+		</section>
+		<section class="doctor doctor3">
+			<img src="images/doctora2.png" alt="doctora2">
+			<article class="descripcion">
+				<h3>Maria Cueva</h3>
+				<p>Snares are silent killers laid by poachers across wild places to trap and kill animals for the illegal bushmeat tradeSnares are silent killers laid by poachers across wild places to trap and kill animals for the illegal bushmeat trade</p>
+			</article>
+		</section>
 	</section>
 	<section class="sponsor">
 		<h3>Sponsor</h3>
